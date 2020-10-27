aadb24a9a7056e89e3b8d6fed15f10eba6704674
pandaman64 <kointosudesuyo@infoseek.jp>
2020-10-27 15:12:32 +0900

    hang repro

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use futures::prelude::*;
+fn main() {
+    // env_logger::Builder::from_default_env()
+    //     .target(env_logger::fmt::Target::Stdout)
+    //     .init();
+    tracing_subscriber::fmt::init();
+
+    let mut runtime = tokio::runtime::Builder::new()
+        .threaded_scheduler()
+        .enable_all()
+        .build()
+        .unwrap();
+    let client: hyper::Client<hyperlocal::UnixConnector> =
+        hyper::Client::builder().build(hyperlocal::UnixConnector);
+    for i in 0.. {
+        println!("{}", i);
+        let res = runtime
+            .block_on(async {
+                let _resp = client
+                    .get(hyperlocal::Uri::new("/var/run/docker.sock", "//events?").into())
+                    .await;
+                client
+                    .get(hyperlocal::Uri::new("/var/run/docker.sock", "/events?").into())
+                    .await
+            })
+            .unwrap();
+        let _handle = runtime.spawn(res.into_body().try_for_each(|_| futures::future::ok(())));
+    }
+}
